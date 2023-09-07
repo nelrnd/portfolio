efe9a8c713af3afbab05a9d117b8155143c4eafe
Nel <nel.renaudin@gmail.com>
2023-09-07 16:25:06 +0200

    Create animations for NavBar and add NavBar backdrop

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,32 +9,36 @@ const NavBar = () => {
   const closeMenu = () => setMenuOpen(false);
 
   return (
-    <header className={`NavBar${menuOpen ? ' opened' : ''}`}>
-      <a href="">
-        <img src={icon} alt="Nel Renaudin" />
-      </a>
+    <>
+      <header className={`NavBar${menuOpen ? ' opened' : ''}`}>
+        <a href="">
+          <img src={icon} alt="Nel Renaudin" />
+        </a>
 
-      <nav role="main">
-        <ul className='nav-links'>
-          <li>
-            <a href="#projects" onClick={closeMenu}>Projects</a>
-          </li>
-          <li>
-            <a href="#about" onClick={closeMenu}>About</a>
-          </li>
-          <li>
-            <a href="#contact" onClick={closeMenu}>Contact</a>
-          </li>
-        </ul>
-      </nav>
+        <nav role="main">
+          <ul className='nav-links'>
+            <li>
+              <a href="#projects" onClick={closeMenu}>Projects</a>
+            </li>
+            <li>
+              <a href="#about" onClick={closeMenu}>About</a>
+            </li>
+            <li>
+              <a href="#contact" onClick={closeMenu}>Contact</a>
+            </li>
+          </ul>
+        </nav>
 
-      <button role="menu" className='menu-btn' onClick={toggleMenu}>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fillRule="evenodd" clipRule="evenodd" d="M2.25 7.25H21.75V8.75H2.25V7.25Z" fill="white" className='bar-1'/>
-          <path fillRule="evenodd" clipRule="evenodd" d="M2.25 15.25H21.75V16.75H2.25V15.25Z" fill="white" className='bar-2'/>
-        </svg>
-      </button>
-    </header>
+        <button role="menu" className='menu-btn' onClick={toggleMenu}>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M2.25 7.25H21.75V8.75H2.25V7.25Z" fill="white" className='bar-1'/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M2.25 15.25H21.75V16.75H2.25V15.25Z" fill="white" className='bar-2'/>
+          </svg>
+        </button>
+      </header>
+
+      {menuOpen && <div className='nav-backdrop' onPointerDown={closeMenu}></div>}
+    </>
   )
 }
 
