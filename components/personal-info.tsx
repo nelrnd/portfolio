8fe780d8419a1f792c09028f37d1872fcbc70a00
Nel <nel.renaudin@gmail.com>
2025-05-31 14:17:39 +0200

    Reduce PersonalInfo title size on mobile

--- components/personal-info.tsx
+++ components/personal-info.tsx
@@ -15,19 +15,23 @@ export default function PersonalInfo() {
       <Grid className="sm:grid-cols-2 bg-background border border-border rounded-2xl px-8 py-12 sm:p-[3.125rem] overflow-hidden">
         <div className="space-y-8 sm:space-y-16">
           <div>
-            <h4 className="uppercase text-soft mb-1">{t("labels.location")}</h4>
+            <h4 className="uppercase text-xs md:text-base text-soft mb-1">
+              {t("labels.location")}
+            </h4>
             <p className="text-xl sm:text-[1.875rem] lg:text-[2.5rem] leading-none">
               {t("data.location")}
             </p>
           </div>
           <div>
-            <h4 className="uppercase text-soft mb-1">{t("labels.time")}</h4>
+            <h4 className="uppercase text-xs md:text-base text-soft mb-1">
+              {t("labels.time")}
+            </h4>
             <p className="text-xl sm:text-[1.875rem] lg:text-[2.5rem] leading-none">
               <Clock timezone={timezone} />
             </p>
           </div>
           <div>
-            <h4 className="uppercase text-soft mb-1">
+            <h4 className="uppercase text-xs md:text-base text-soft mb-1">
               {t("labels.availability")}
             </h4>
             <p className="text-xl sm:text-[1.875rem] lg:text-[2.5rem] leading-none">
